224b9b90177b9f4cb227978fa77757debf884ec8
dangthevang <dangthevang2000@gmail.com>
2022-04-07 15:08:15 +0700

    FixPlayer

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -3,4 +3,5 @@ from gym_splendor.envs.agents import agent_2 as a2
 from gym_splendor.envs.agents import agent_3 as a3
 from gym_splendor.envs.agents import agent_4 as a4
 
-ListPlayer = [a1.Agent("1"),a1.Agent("2"),a1.Agent("3"),a1.Agent("4")]
+ListPlayer = [a1.Agent("1"),a2.Agent("2")]
+# ,a1.Agent("3"),a1.Agent("4")
--- setup.py
+++ setup.py
@@ -4,8 +4,3 @@ setup(name='gym_splendor',
     version='0.0.2',
     install_requires=['gym','colorama']
 )
-
-
-
-
-
