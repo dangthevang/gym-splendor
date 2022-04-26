b747b909af48ebf4a36dfd68489bf0ab27112154
DESKTOP-TRB8SVB\Admin <tranxuanhieuhust@gmail.com>
2022-04-26 16:58:50 +0700

    HI

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -3,5 +3,5 @@ from gym_splendor.envs.agents import agent_random_lomdom as a2
 from gym_splendor.envs.agents import agent_random_lomdom as a3
 from gym_splendor.envs.agents import agent_random_lomdom as a4
 
-ListPlayer = [a1.Agent("Hieumoi"),a2.Agent("random"),a3.Agent("random"),a4.Agent("random")]
+ListPlayer = [a1.Agent("Hieumoi"),a2.Agent("random"),a3.Agent("random2"),a4.Agent("random3")]
 # 
