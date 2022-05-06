ee6d7b5e43d2707a53a40fafe97a08b6ca1c122f
minhanh1135 <64486608+minhanh1135@users.noreply.github.com>
2022-05-06 20:04:10 +0700

    commit 4 bot random

--- gym_splendor/envs/agents/agents_inteface.py
+++ gym_splendor/envs/agents/agents_inteface.py
@@ -3,9 +3,9 @@
 # from gym_splendor.envs.agents import agent_3 as a3
 # from gym_splendor.envs.agents import agent_test as a4
 
-from gym_splendor.envs.agents import agent_1 as a1
-from gym_splendor.envs.agents import agent_2 as a2
-from gym_splendor.envs.agents import agent_3 as a3
+from gym_splendor.envs.agents import agent_test as a1
+from gym_splendor.envs.agents import agent_test as a2
+from gym_splendor.envs.agents import agent_test as a3
 from gym_splendor.envs.agents import agent_test as a4
 
 # from gym_splendor.envs.agents import agent_1 as a1
