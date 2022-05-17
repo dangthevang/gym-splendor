cf30a89d06b7e11e2f37d2b0aec23034f09056b6
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-05-17 11:30:04 +0700

    no message

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import gym
 import gym_splendor
 import pandas as pd
 import time
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 def main():
     env = gym.make('gym_splendor-v0')
@@ -10,7 +12,7 @@ def main():
     start_time = time.time()
     while env.turn <1000:
         o,a,done,t = env.step(env.player[env.turn%env.amount_player].action(state = env.state))
-        # env.render()
+        env.render()
         if done == True:
             break
     for i in range(4):
