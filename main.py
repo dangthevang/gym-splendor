9b1c2bca09413871d620fe68a88381d632d71a18
01001101011010010110010001100001 <anhdo@vis-laboratory.com>
2022-05-24 10:48:51 +0000

    NA model training parallel

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ import gym
 import gym_splendor
 import pandas as pd
 import time
+from contextlib import contextmanager
+import sys, os
 
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
 def main():
     env = gym.make('gym_splendor-v0')
     env.reset() 
@@ -18,5 +29,34 @@ def main():
         o,a,done,t = env.step(env.player[env.turn%env.amount_player].action(env.state))
     # print(env.pVictory)
     print(time.time()-start_time)
-if __name__ == '__main__':
-    main()
+
+# main()
+def run1():
+    with suppress_stdout():
+        name = main()
+    print(name)
+
+
+import multiprocessing
+if __name__ != "main":
+    start_time = time.perf_counter()
+ 
+    # Creates two processes
+    p1 = multiprocessing.Process(target=run1)
+    p2 = multiprocessing.Process(target=run1)
+    p3 = multiprocessing.Process(target=run1)
+    p4 = multiprocessing.Process(target=run1)
+    p5 = multiprocessing.Process(target=run1)
+    p6 = multiprocessing.Process(target=run1)
+ 
+    # Starts both processes
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+    p5.start()
+    p6.start()
+ 
+    finish_time = time.perf_counter()
+ 
+    print(f"Program finished in {finish_time-start_time} seconds")
