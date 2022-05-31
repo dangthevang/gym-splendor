b0536cf54f00329fe364057c60316ee56c149dd7
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-05-31 14:32:59 +0700

    hi

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import numpy as np
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
+start_time = time.time()
 def main():
     env = gym.make('gym_splendor-v0')
     env.reset() 
-    start_time = time.time()
     while env.turn <1000:
         o,a,done,t = env.step(env.player[env.turn%env.amount_player].action(env.state))
         # env.render()
@@ -22,6 +22,7 @@ def main():
         o,a,done,t = env.step(env.player[env.turn%env.amount_player].action(env.state))
     # print(env.pVictory)
     print(env.turn, env.pVictory.name, env.pVictory.score)
-    # print(time.time()-start_time)
-if __name__ == '__main__':
-    main()
+for i in range(15):
+    if __name__ == '__main__':
+        main()
+print(time.time()-start_time)
